ae2e6c5d66be0f4767e8744ccc035a45c47b484b
Dustin Newman <dustinnewman98@gmail.com>
2019-10-24 10:11:49 -0700

    fixed prefix paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,12 @@ const siteName = 'Subtle Asian Traits | Daily Bruin';
 const description = 'The Facebook group Subtle Asian Traits amasses thousands of memes and posts about Asian culture, providing a hub for its 1.5 million members to relate over a shared identity.';
 const image = 'https://assets.dailybruin.com/images/flatpage.2019.subtle-asian-traits/illo-5bd9c2eb93f6d5f92a6dbf296ebcfc6c.jpg';
 const year = '2019';
+const prefixPath = `/${year}/subtle-asian-traits`;
 
-const url = `https://features.dailybruin.com/${year}/subtle-asian-traits`;
+const url = `https://features.dailybruin.com${prefixPath}/`;
 
 module.exports = {
+  pathPrefix: prefixPath,
   siteMetadata: {
     siteName,
     description,

396376d4b61bd5673ff8830bd0df5e83284811c0
Dustin Newman <dustinnewman98@gmail.com>
2019-10-24 10:35:25 -0700

    idk whats wrong with video i got rid of plyr css and comments and extra imported components from lux in index page

--- src/components/CustomVideo.tsx
+++ src/components/CustomVideo.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { Video } from '@dailybruin/lux';
-// tslint:disable-next-line
-import 'plyr/dist/plyr.css';
 
 export interface VideoProps {
   /** The id of the YouTube video. */
@@ -9,6 +7,7 @@ export interface VideoProps {
 }
 
 export default function CustomVideo(props: VideoProps) {
+  console.log("Video " + props.videoId)
   return <div style={{
     margin: "0px 0px 25px 0px",
   }}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,7 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import {
   Article,
-  CoverPhoto,
-  PullQuote,
-  Head,
-  XPosition,
-  YPosition,
+  Head
 } from '@dailybruin/lux'
 import Header from '../components/Header';
 import colors from '../utils/colors';

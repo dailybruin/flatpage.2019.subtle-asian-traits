107e287b1f9a43fcec88737d67dc89ea2a4cdd28
Dustin Newman <dustinnewman98@gmail.com>
2019-10-24 10:27:23 -0700

    video

--- src/components/CustomVideo.tsx
+++ src/components/CustomVideo.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react'
-import ReactPlayer from 'react-player'
-import {Video} from '@dailybruin/lux'
+import * as React from 'react';
+import { Video } from '@dailybruin/lux';
 // tslint:disable-next-line
-import 'plyr/dist/plyr.css'
+import 'plyr/dist/plyr.css';
 
 export interface VideoProps {
   /** The id of the YouTube video. */
@@ -11,10 +10,9 @@ export interface VideoProps {
 
 export default function CustomVideo(props: VideoProps) {
   return <div style={{
-            margin: "0px 0px 25px 0px",
-          }}>
-            {/* <ReactPlayer width="100%" url={'https://www.youtube.com/watch?v=' + props.videoId} controls/> */}
-            <Video videoId={props.videoId}/>
-          </div>
-          
+    margin: "0px 0px 25px 0px",
+  }}>
+    <Video videoId={props.videoId} />
+  </div>
+
 }
